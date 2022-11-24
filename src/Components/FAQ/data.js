e4cb6f37ccade826a36261a516298231e899a28e
Samiha Hossain <75538711+Samiha-Hossain19@users.noreply.github.com>
2022-11-24 21:13:37 +0600

    initial commit

---
+++ src/Components/FAQ/data.js
@@ -0,0 +1,22 @@
+export const faqsData = [
+    {
+      id: 1,
+      title: "How can we reach you?",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis dicta nihil neque molestias cupiditate obcaecati officiis magnam numquam iure doloremque.",
+    },
+    {
+      id: 2,
+      title: "When you are available?",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis dicta nihil neque molestias cupiditate obcaecati officiis magnam numquam iure doloremque.",
+    },
+    {
+      id: 3,
+      title: "How can I make an appointment?",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis dicta nihil neque molestias cupiditate obcaecati officiis magnam numquam iure doloremque.",
+    },
+    {
+      id: 4,
+      title: "Where can I get all the information?",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis dicta nihil neque molestias cupiditate obcaecati officiis magnam numquam iure doloremque.",
+    },
+  ];
